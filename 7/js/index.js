4d3ec25ab860c2f604fd3e184b77f346fe3243df
Keks <keks@htmlacademy.ru>
2021-02-07 23:21:03 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/index.js
+++ 7/js/index.js
@@ -1,51 +1,141 @@
-const slide = document.querySelector('img.slider');
-const sliderImgSrc = ["img/slider_perforators.jpg", "img/slider_drill.jpg"];
+// PROMO_SLIDER
+
+const dots = document.querySelectorAll('.slider-dot-item');
+const arrowLeft = document.querySelector('.slider-arrow-left');
+const arrowRight = document.querySelector('.slider-arrow-right');
+const promoSlide = document.querySelector(".promo-slide-img img");
+const sliderHeaderCurent = document.querySelector(".promo-slide-name");
+const sliderTextCurent = document.querySelector(".promo-slide-description");
+
+const sliderImgSrc = [
+  "img/slides/slide-perforator.jpg",
+   "img/slides/slide-drill.jpg",
+  //  "G:\html_academy\1661607-technomart-30\img\slides\slide-perforator.jpg"
+  ];
+const sliderHeaders = [
+  "ПЕРФОРАТОРЫ",
+   "ДРЕЛИ"
+  ];
+const sliderTexts = [
+  "Настоящие мужские игрушки", 
+  "Соседям на радость!"
+]
 let slideId = 0
 
-slide.addEventListener('click', () => {
+const changeSlide = (num) => {
+  dots.forEach(dot => {
+    dot.classList.remove("slider-dot-active");
+  });
+  dots[num].classList.add("slider-dot-active");
+  promoSlide.setAttribute('src', sliderImgSrc[num])
+  sliderHeaderCurent.textContent = sliderHeaders[num];
+  sliderTextCurent.textContent = sliderTexts[num];
+}
+
+arrowRight.addEventListener('click', () => {
   if (slideId < sliderImgSrc.length - 1) {
     slideId++;
   } else {
     slideId = 0
   };
-  slide.setAttribute('src', sliderImgSrc[slideId]);
+  changeSlide(slideId);
+})
+
+arrowLeft.addEventListener('click', () => {
+  if (slideId > 0) {
+    slideId--;
+  } else {
+    slideId = sliderImgSrc.length - 1;
+  };
+  changeSlide(slideId);
+});
+
+dots.forEach((dot, i) => {
+  dot.addEventListener('click', (evt) => {
+    if (evt.target == dot) changeSlide(i);
+  })
 })
 
 
+// services-slider
 
 const srviceTexts = [
   'Мы с удовольствием доставим ваш товар прямо <br />к вашему подъезду совершенно бесплатно!<br />Ведь мы неплохо заработаем, <br />поднимая его на ваш этаж!',
   'Если купленный у нас товар поломается или заискрит, а также в случае пожара, спровоцированного его возгоранием, вы всегда можете быть уверены в нашей гарантии. Мы обменяем сгоревший товар на новый. Дом уж восстановите как-нибудь сами.',
   'Залезть в долговую яму стало проще! <br />Кредитные консультанты придут вам на помощь.'
 ]
+const srviceImgClases = [
+  'service-img-truck', 
+  'service-img-garant', 
+  'service-img-credit'
+];
 
-const srviceImgClases = ['serv_img0', 'serv_img1', 'serv_img2']
+const services = document.querySelectorAll('.services-item');
+const headerService = document.querySelector('.service-description h3');
+const textService = document.querySelector('.service-description p');
+const serviceImg = document.querySelector('.service-img');
+const buttonCredit = document.querySelector('.button-credit');
 
-const services = document.querySelectorAll('.serv-items');
-const headerServ = document.querySelector('.text_services h3')
-const textServPar = document.querySelector('.text_services p')
-const servImg = document.querySelector('.services_img')
+const allDisactivate = () => {
+  services.forEach(service => {
+    service.classList.remove('services-item-active');
+    service.classList.add('services-item-disactive');
+  })
+}
 
 services.forEach((serv, i) => {
   serv.addEventListener('click', () => {
-    services.forEach(s => s.classList.remove('serv-items-active'));
-    serv.classList.add('serv-items-active');
-    headerServ.textContent = serv.textContent;
-    textServPar.innerHTML = srviceTexts[i];
-    srviceImgClases.forEach(clas => servImg.classList.remove(clas))
-    servImg.classList.add(srviceImgClases[i]);
+    allDisactivate();
+    serv.classList.remove('services-item-disactive');
+    serv.classList.add('services-item-active');
+    headerService.textContent = serv.textContent;
+    textService.innerHTML = srviceTexts[i];
+    srviceImgClases.forEach(clas => serviceImg.classList.remove(clas))
+    serviceImg.classList.add(srviceImgClases[i]);
+    if (serv.textContent === "Кредит") {
+      buttonCredit.style.display = "block";
+    } else {
+      buttonCredit.style.display = "none";
+    }
   });
 })
 
 
-const writeUs = document.getElementById('write_us');
-const modalCalling = document.querySelector('.modal_calling');
-const closeModalBtn = modalCalling.querySelector('.close_basket')
+// modals
 
+//!!! 
+// при открытии любого модального окна, страница дергается, наверно, из-за ползунка. Будет время, разберусь - поправлю 
+// !!!
 
-writeUs.addEventListener('click', evt => {
-  evt.preventDefault();
-  modalCalling.parentNode.style.display = 'flex';
-})
+function openModal(modalForOpen, btnForOpen) {
+  btnForOpen.addEventListener('click', evt => {
+    evt.preventDefault();
+    modalForOpen.parentNode.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+  })
+}
 
-closeModalBtn.onclick = () => modalCalling.parentNode.style.display = 'none';
+function closeModal(modalForClose, btnForClose) {
+  btnForClose.addEventListener('click', () => {
+    modalForClose.parentNode.style.display = 'none';
+    document.body.style.overflow = '';
+  })
+}
+
+// login modal
+
+const writeUs = document.getElementById('write-us');
+const modalLogin = document.querySelector('.modal-login');
+const closeModalLogin = modalLogin.querySelector('.modal-close');
+
+openModal(modalLogin, writeUs);
+closeModal(modalLogin, closeModalLogin);
+
+// map modal
+
+const mapButton = document.querySelector('.company-location-img');
+const modalMap = document.querySelector('.modal-map');
+const closeModalMap = modalMap.querySelector('.modal-close');
+
+openModal(modalMap, mapButton);
+closeModal(modalMap, closeModalMap);
