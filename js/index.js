f5653c7123a661fa3e4b38434b771ff788c0f60d
Sergey <redmoloch@yanadex.ru>
2021-02-07 16:07:00 +0300

    made promo-slider in js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,55 @@
-const slide = document.querySelector('img.slider');
-const sliderImgSrc = ["img/slider_perforators.jpg", "img/slider_drill.jpg"];
+const dots = document.querySelectorAll('.slider-dot-item');
+const arrowLeft = document.querySelector('.slider-arrow-left');
+const arrowRight = document.querySelector('.slider-arrow-right');
+const slide = document.querySelector(".promo-slide-img img");
+const sliderHeaderCurent = document.querySelector(".promo-slide-name");
+const sliderTextCurent = document.querySelector(".promo-slide-description");
+
+const sliderImgSrc = [
+  "img/slides/slide-perforator.jpg",
+   "img/slides/slide-drill.jpg",
+  //  "G:\html_academy\1661607-technomart-30\img\slides\slide-perforator.jpg"
+  ];
+const sliderHeaders = [
+  "ПЕРФОРАТОРЫ",
+   "ДРЕЛИ"
+  ];
+const sliderTexts = [
+  "Настоящие мужские игрушки", 
+  "Соседям на радость!"
+]
 let slideId = 0
 
-slide.addEventListener('click', () => {
+
+
+const changeSlide = (num) => {
+  dots.forEach(dot => {
+    dot.classList.remove("slider-dot-active");
+  });
+  dots[num].classList.add("slider-dot-active");
+  slide.setAttribute('src', sliderImgSrc[num])
+  sliderHeaderCurent.textContent = sliderHeaders[num];
+  sliderTextCurent.textContent = sliderTexts[num];
+
+}
+
+arrowRight.addEventListener('click', () => {
   if (slideId < sliderImgSrc.length - 1) {
     slideId++;
   } else {
     slideId = 0
   };
-  slide.setAttribute('src', sliderImgSrc[slideId]);
+  changeSlide(slideId);
 })
 
-
+arrowLeft.addEventListener('click', () => {
+  if (slideId > 0) {
+    slideId--;
+  } else {
+    slideId = sliderImgSrc.length - 1;
+  };
+  changeSlide(slideId);
+})
 
 const srviceTexts = [
   'Мы с удовольствием доставим ваш товар прямо <br />к вашему подъезду совершенно бесплатно!<br />Ведь мы неплохо заработаем, <br />поднимая его на ваш этаж!',
@@ -19,33 +57,33 @@ const srviceTexts = [
   'Залезть в долговую яму стало проще! <br />Кредитные консультанты придут вам на помощь.'
 ]
 
-const srviceImgClases = ['serv_img0', 'serv_img1', 'serv_img2']
+// const srviceImgClases = ['serv_img0', 'serv_img1', 'serv_img2']
 
-const services = document.querySelectorAll('.serv-items');
-const headerServ = document.querySelector('.text_services h3')
-const textServPar = document.querySelector('.text_services p')
-const servImg = document.querySelector('.services_img')
+// const services = document.querySelectorAll('.serv-items');
+// const headerServ = document.querySelector('.text_services h3')
+// const textServPar = document.querySelector('.text_services p')
+// const servImg = document.querySelector('.services_img')
 
-services.forEach((serv, i) => {
-  serv.addEventListener('click', () => {
-    services.forEach(s => s.classList.remove('serv-items-active'));
-    serv.classList.add('serv-items-active');
-    headerServ.textContent = serv.textContent;
-    textServPar.innerHTML = srviceTexts[i];
-    srviceImgClases.forEach(clas => servImg.classList.remove(clas))
-    servImg.classList.add(srviceImgClases[i]);
-  });
-})
-
-
-const writeUs = document.getElementById('write_us');
-const modalCalling = document.querySelector('.modal_calling');
-const closeModalBtn = modalCalling.querySelector('.close_basket')
+// services.forEach((serv, i) => {
+//   serv.addEventListener('click', () => {
+//     services.forEach(s => s.classList.remove('serv-items-active'));
+//     serv.classList.add('serv-items-active');
+//     headerServ.textContent = serv.textContent;
+//     textServPar.innerHTML = srviceTexts[i];
+//     srviceImgClases.forEach(clas => servImg.classList.remove(clas))
+//     servImg.classList.add(srviceImgClases[i]);
+//   });
+// })
 
 
-writeUs.addEventListener('click', evt => {
-  evt.preventDefault();
-  modalCalling.parentNode.style.display = 'flex';
-})
+// const writeUs = document.getElementById('write_us');
+// const modalCalling = document.querySelector('.modal_calling');
+// const closeModalBtn = modalCalling.querySelector('.close_basket')
 
-closeModalBtn.onclick = () => modalCalling.parentNode.style.display = 'none';
+
+// writeUs.addEventListener('click', evt => {
+//   evt.preventDefault();
+//   modalCalling.parentNode.style.display = 'flex';
+// })
+
+// closeModalBtn.onclick = () => modalCalling.parentNode.style.display = 'none';
